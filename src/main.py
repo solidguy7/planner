3012c9ca2683a1caee7b55de2dcc5ea2d302f805
solidguy7 <kirill15grishkov01@gmail.com>
2023-03-04 14:11:55 +0300

    CRUD with MongoDB

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
-from database.connection import conn
 from routes.users import user_router
 from routes.events import event_router
+from database.connection import Settings
 import uvicorn
 
 app = FastAPI()
+
+settings = Settings()
 
 app.include_router(user_router, prefix='/user')
 app.include_router(event_router, prefix='/event')
 
 @app.on_event('startup')
-def on_startup():
-    conn()
+async def init_db():
+    await settings.initialize_database()
 
 @app.get('/')
 async def home():
--- src/routes/users.py
+++ src/routes/users.py
@@ -1,21 +1,24 @@
 from fastapi import APIRouter, HTTPException, status
+from database.connection import Database
 from models.users import User, UserSignIn
+
+user_database = Database(User)
 
 user_router = APIRouter(tags=['User'])
 
-users = {}
-
 @user_router.post('/signup')
-async def sign_new_user(data: User) -> dict:
-    if data.email in users:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='User with supplied username exists')
-    users[data.email] = data.password
-    return {'message': 'User successfully registered!'}
+async def sign_user_up(user: User) -> dict:
+    user_exist = await User.find_one(User.email == user.email)
+    if user_exist:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='User with email provided exists already.')
+    await user_database.save(user)
+    return {'message': 'User created successfully'}
 
 @user_router.post('/signin')
 async def sign_user_in(user: UserSignIn) -> dict:
-    if user.email not in users:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User does not exist')
-    if users[user.email] != user.password:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Wrong credentials passed')
+    user_exist = await User.find_one(User.email == user.email)
+    if not user_exist:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User with email does not exist.')
+    if user_exist.password != user.password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid details passed.')
     return {'message': 'User signed in successfully'}
